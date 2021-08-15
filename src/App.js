0ad1c67f770748d990aac6bee6e4a9d73fc7fb69
Alex Norton <atn362@gmail.com>
2021-08-15 15:14:11 -0500

    added card pic

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <a href="https://www.nortonfloors.com/">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" target="_blank" alt="logo" />
         </a>
         <p>
           Edit <code>src/App.js</code> and save to reload.
